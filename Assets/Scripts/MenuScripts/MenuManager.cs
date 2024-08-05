23dd5248d1513c2ebcc997637e596063cfc680ec
Fatma Gül Gök <148465338+fatmagulgk@users.noreply.github.com>
2024-08-05 06:47:06 +0300

    Revert "menu design"
    This reverts commit de5a1e0472aa9bb4a3df5d99bb070f02d570295e.

--- Assets/Scripts/MenuScripts/MenuManager.cs
+++ Assets/Scripts/MenuScripts/MenuManager.cs
@@ -1,27 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    [SerializeField] private Transform Head;
-    [SerializeField] private Transform StartButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
-    private void Start()
+    // Update is called once per frame
+    void Update()
     {
-        HeadDisplacement();
-        StartButton.GetComponent<Button>().onClick.AddListener(StartGame);
-    }
-    public void HeadDisplacement()
-    {
-        Head.transform.GetComponent<RectTransform>().DOLocalMoveX(0f,1f).SetEase(Ease.OutBack);
-        StartButton.transform.GetComponent<RectTransform>().DOLocalMoveY(-250f,1f).SetEase(Ease.OutBack);
-    }
-    public void StartGame()
-    {
-        SceneManager.LoadScene("Game");
+        
     }
 }
