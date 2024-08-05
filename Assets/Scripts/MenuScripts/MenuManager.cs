e696d123ecd8175fcab467c5da715b095865e86b
Fatma Gül Gök <148465338+fatmagulgk@users.noreply.github.com>
2024-08-05 06:48:55 +0300

    Reapply "menu design"
    This reverts commit 23dd5248d1513c2ebcc997637e596063cfc680ec.

--- Assets/Scripts/MenuScripts/MenuManager.cs
+++ Assets/Scripts/MenuScripts/MenuManager.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform Head;
+    [SerializeField] private Transform StartButton;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        HeadDisplacement();
+        StartButton.GetComponent<Button>().onClick.AddListener(StartGame);
+    }
+    public void HeadDisplacement()
+    {
+        Head.transform.GetComponent<RectTransform>().DOLocalMoveX(0f,1f).SetEase(Ease.OutBack);
+        StartButton.transform.GetComponent<RectTransform>().DOLocalMoveY(-250f,1f).SetEase(Ease.OutBack);
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
     }
 }
